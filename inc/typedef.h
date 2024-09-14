204aa1f61eb8d2c8e8909f975f746c0269479c57
qyz <917768104@qq.com>
2024-09-14 16:14:25 +0800

    fix typedef.h

--- inc/typedef.h
+++ inc/typedef.h
@@ -8,37 +8,28 @@
 //---------------------------------------
 #ifndef __TYPEDEF_H__
 #define __TYPEDEF_H__
-//---------------------------------------
-typedef signed char  		s8;
-typedef signed short 		s16;
-typedef signed long  		s32;
-typedef signed long long  	s64;
 
-typedef unsigned char  		u8;
-typedef unsigned short 		u16;
-typedef unsigned long  		u32;
+#include <stdbool.h>
+
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed long         s32;
+typedef signed long long    s64;
+
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned long       u32;
 typedef unsigned long long  u64;
 
-typedef float				f32;
-typedef double				f64;
+typedef float               f32;
+typedef double              f64;
 
-#ifndef bool
-typedef enum {FALSE = 0, TRUE = !FALSE} bool;
-#endif
 
 typedef void (*PHOOK_t)(void);
 
 #ifndef NULL
-#define NULL        (void *)0
+#define NULL                (void *)0
 #endif
 
-#ifndef true
-#define true        1
-#endif
-
-#ifndef false
-#define false       0
-#endif
-//---------------------------------------
 #endif
 
